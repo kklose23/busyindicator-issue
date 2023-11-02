041ba86c1fdc3bbd118054263296bbdfa16d84b3
Kevin Klose <kevin.klose@teamdynamix.com>
2023-11-02 13:28:46 -0400

    first commit

---
+++ MonkeyFinder/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui;
+using MonkeyFinder.Services;
+using MonkeyFinder.View;
+using Telerik.Maui.Controls.Compatibility;
+
+namespace MonkeyFinder;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+      .UseTelerik()
+      .UseMauiCommunityToolkit()
+      .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+#if DEBUG
+		//builder.Logging.AddDebug();
+#endif
+
+    builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
+		
+		builder.Services.AddSingleton<MonkeyService>();
+		builder.Services.AddSingleton<MonkeysViewModel>();
+		builder.Services.AddSingleton<MainPage>();
+
+		return builder.Build();
+	}
+}
---
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MonkeyFinder.View;
+
+public partial class MainPage : ContentPage
+{
+  public MainPage(MonkeysViewModel viewModel)
+  {
+    InitializeComponent();
+    BindingContext = viewModel;
+  }
+
+  private void Button_Clicked(object sender, EventArgs e)
+  {
+    busyIndicator.IsBusy = true;
+    grid.HeightRequest = 500;
+    busyIndicator.IsBusy = false;
+  }
+}
+
